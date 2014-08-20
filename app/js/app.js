5d5896b216e2e69daed910c6fba78dffad7a4415
Alex Kates <daniel.alexander.kates@gmail.com>
2014-08-20 16:54:26 -0400

    implemented google+ login implemented bower as dependency manager

--- app/js/app.js
+++ app/js/app.js
@@ -2,15 +2,34 @@
 angular.module('drankly.controllers', ['drankly.services']);
 
 var app = angular.module('drankly', [
-	'geolocation',
-	'google-maps',
-	'ngGPlaces',
-	'drankly.controllers',
-	'drankly.services'
+    'ngRoute',
+    'googleplus',
+    'geolocation',
+    'google-maps',
+    'ngGPlaces',
+    'drankly.controllers',
+    'drankly.services'
 ]);
 
-app.config(function(ngGPlacesAPIProvider) {
-	ngGPlacesAPIProvider.setDefaults({
-		nearbySearchKeys: ['name', 'geometry', 'reference']
-	});
+app.config(function($routeProvider, GooglePlusProvider, ngGPlacesAPIProvider) {
+    GooglePlusProvider.init({
+        clientId: '672033689369-vektsidribcffig4r9u2amf8qce67ecc'
+    });
+
+    $routeProvider.when("/", {
+        templateUrl: 'app/partials/app.html'
+    }).when("/login", {
+        templateUrl: 'app/partials/login.html',
+        controller: 'LoginCtrl'
+    });
+
+    ngGPlacesAPIProvider.setDefaults({
+        nearbySearchKeys: ['name', 'geometry', 'reference']
+    });
+}).run(function($rootScope, $location) {
+    $rootScope.$on("$routeChangeStart", function(event, next, current) {
+        if ($rootScope.user == null)
+            if (next.templateUrl != "partials/login.html")
+                $location.path("/login");
+    });
 });
